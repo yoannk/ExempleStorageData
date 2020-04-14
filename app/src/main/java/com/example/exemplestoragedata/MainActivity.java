3b42678db81821118f381d174dd75ab25c39146b
Yoann Kergall <yoann.kergall@gmail.com>
2020-04-14 10:24:25 +0200

    ajout save data

--- app/src/main/java/com/example/exemplestoragedata/MainActivity.java
+++ app/src/main/java/com/example/exemplestoragedata/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.exemplestoragedata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+
+        Button btnSaveInternalStorage = findViewById(R.id.btnSaveInternalStorage);
+
+        btnSaveInternalStorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomFichier = "FichierData.txt";
+
+                String data = "Bonjour toto !";
+
+                FileOutputStream fos;
+
+                try {
+                    // on ouvre un fichier pour écrire à l'intérieur
+                    fos = openFileOutput(nomFichier, MODE_PRIVATE);
+                    // on écrit nos données
+                    fos.write(data.getBytes());
+                    // on ferme le fichier
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    Log.e("Erreur Data", e.getMessage());
+                } catch (IOException e) {
+                    Log.e("Erreur Data", e.getMessage());
+                }
+            }
+        });
     }
 }
