15a9baf828a0be2c3b5744cb505eb1364bcf3d38
Yoann Kergall <yoann.kergall@gmail.com>
2020-04-14 10:53:01 +0200

    ajout entité Adherent

---
+++ app/src/main/java/com/example/exemplestoragedata/Entities/Adherent.java
@@ -0,0 +1,22 @@
+package com.example.exemplestoragedata.Entities;
+
+public class Adherent {
+    private String nom;
+    private String prenom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+}
